ce6a9e1971d015106b77d41d806ca8daed1d5b2a
Antonio Fry <antoniofry2000@gmail.com>
2020-09-25 15:50:14 -0600

    Create mock data for expenses

---
+++ src/Data/mockExpenseData.js
@@ -0,0 +1,20 @@
+export const mockExpenseData = [
+  {
+    id: 1,
+    amount: 23.00,
+    accountId: 1,
+    categoryId: 2 
+  },
+  {
+    id: 2,
+    amount: 60.00,
+    accountId: 1,
+    categoryId: 1
+  },
+  {
+    id: 3,
+    amount: 17.00,
+    accountId: 2,
+    categoryId: 3,
+  }
+];
