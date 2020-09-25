87a6052959eecb598d464b6bf5b58afa8e25629e
Antonio Fry <antoniofry2000@gmail.com>
2020-09-25 12:00:28 -0600

    Build out main layout for landing page in App

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -7,22 +7,19 @@ class App extends Component {
 
   render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <main>
+        <header className="page-header">
+
         </header>
-      </div>
+        <section className="landing-page-container">
+          <article className="project-info-container">
+            
+          </article>
+          <footer className="external-links-container">
+            
+          </footer>
+        </section>
+      </main>
     );
   }
 }
