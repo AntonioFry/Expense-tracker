09a7fb31f71f3a0893bb87c08bfee659b1b6e0e2
Antonio Fry <antoniofry2000@gmail.com>
2020-09-25 14:02:29 -0600

    Create method and conditional to alter which page is toggled

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
 import LandingPage from '../LandingPage/LandingPage';
 import './App.css';
+import ExpenseDashboard from '../ExpenseDashboard/ExpenseDashboard';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      pageToggled: 'landing page'
+    }
+  }
 
+  changePage = (pageName) => {
+    this.setState({ pageToggled: pageName });
+  }
 
   render () {
     return (
@@ -11,11 +21,12 @@ class App extends Component {
         <header className="page-header">
           <h1 className="header-title">Expense Tracker</h1>
           <div className="nav-button-container">
-            <button className="nav-button">Home</button>
-            <button className="nav-button">Dashboard</button>
+            <button className="nav-button" onClick={() => this.changePage('landing page')}>Home</button>
+            <button className="nav-button" onClick={() => this.changePage('dashboard')}>Dashboard</button>
           </div>
         </header>
-        <LandingPage />
+        { this.state.pageToggled === 'landing page' ? <LandingPage /> : null }
+        { this.state.pageToggled === 'dashboard' ? <ExpenseDashboard /> : null }
       </main>
     );
   }
