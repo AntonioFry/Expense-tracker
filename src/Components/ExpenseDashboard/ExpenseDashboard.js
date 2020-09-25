6006c4984efca6f3c25f75b9b9d5b99739882e98
Antonio Fry <antoniofry2000@gmail.com>
2020-09-25 13:50:24 -0600

    Initialize and create layout for ExpenseDahboard

---
+++ src/Components/ExpenseDashboard/ExpenseDashboard.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './ExpenseDashboard.css';
+
+class ExpenseDashboard extends Component {
+  constructor() {
+    super();
+    this.state = {
+      expenseData: [],
+    }
+  }
+
+  render() {
+    return (
+      <section className='expense-dashboard-container'>
+        
+      </section>
+    )
+  }
+};
+
+export default ExpenseDashboard;
