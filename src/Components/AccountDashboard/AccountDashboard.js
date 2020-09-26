eed47384331632ff7eda89cc84a9410690c2f56a
Antonio Fry <antoniofry2000@gmail.com>
2020-09-26 13:09:35 -0600

    Initialize AccountDashboard files

---
+++ src/Components/AccountDashboard/AccountDashboard.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class AccountDashboard extends Component {
+
+  render() {
+    return (
+      <section className="account-dashboard-section">
+
+      </section>
+    )
+  }
+}
