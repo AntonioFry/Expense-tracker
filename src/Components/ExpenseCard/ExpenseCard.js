ce157048218f621097163e6cab9bace7e663c179
Antonio Fry <antoniofry2000@gmail.com>
2020-09-25 19:34:36 -0600

    Revert ExpenseCard back to a functional component

--- src/Components/ExpenseCard/ExpenseCard.js
+++ src/Components/ExpenseCard/ExpenseCard.js
@@ -1,29 +1,15 @@
 import React, { Component } from 'react';
 import './ExpenseCard.css';
 
-class ExpenseCard extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      id: this.props.id,
-      account: this.props.account,
-      category: this.props.category,
-      amount: this.props.amount,
-      date: this.props.date,
-    }
-  }
-
-  render () { 
-    const { date, account, category, amount } = this.state;
-    return (
-        <tr>
-          <td className="table-box">{date}</td>
-          <td className="table-box">{account}</td>
-          <td className="table-box">${amount}</td>
-          <td className="table-box">{category}</td>
-        </tr>
-    )
-  }
+const ExpenseCard = ({ date, account, category, amount }) => {
+  return (
+      <tr>
+        <td className="table-box">{date}</td>
+        <td className="table-box">{account}</td>
+        <td className="table-box">${amount}</td>
+        <td className="table-box">{category}</td>
+      </tr>
+  )
 }
 
 export default ExpenseCard
