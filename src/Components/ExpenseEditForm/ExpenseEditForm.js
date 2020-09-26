2f20a27982231a5c54d78021645472649ce5d42d
Antonio Fry <antoniofry2000@gmail.com>
2020-09-25 23:33:04 -0600

    Style layout of edit form

--- src/Components/ExpenseEditForm/ExpenseEditForm.js
+++ src/Components/ExpenseEditForm/ExpenseEditForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './ExpenseEditForm.css';
 
 class ExpenseEditForm extends Component {
   constructor(props) {
@@ -17,36 +18,37 @@ class ExpenseEditForm extends Component {
 
   render() {
     return (
-      <form>
+      <form className='expense-edit-form'>
         <h2>Editing Expense #{this.props.id}</h2>
-        <label>Account</label>
+        <label className="form-labels">Account</label>
         <input
           className="edit-expense-input"
           name="account"
           value={this.state.account}
           onChange={(e) => this.handleChange(e)}
         />
-        <label>Date</label>
+        <label className="form-labels">Date</label>
         <input
           className="edit-expense-input"
           name="date"
           value={this.state.date}
           onChange={(e) => this.handleChange(e)}
         />
-        <label>Amount</label>
+        <label className="form-labels">Amount</label>
         <input
           className="edit-expense-input"
           name="amount"
           value={this.state.amount}
           onChange={(e) => this.handleChange(e)}
         />
-        <label>Category</label>
+        <label className="form-labels">Category</label>
         <input
           className="edit-expense-input"
           name="category"
           value={this.state.category}
           onChange={(e) => this.handleChange(e)}
         />
+        <button className="submit-change-btn">Submit Changes</button>
       </form>
     )
   }
