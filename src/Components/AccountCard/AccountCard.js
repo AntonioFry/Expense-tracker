93ba424feea545a47b101b09031f9d078757fc81
Antonio Fry <antoniofry2000@gmail.com>
2020-09-26 14:37:33 -0600

    Render AccountCards in AccountDashboard

--- src/Components/AccountCard/AccountCard.js
+++ src/Components/AccountCard/AccountCard.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
 const AccountCard = ({ id, title, color, type }) => {
+  const headerColorStyle = {
+    'backgroundColor': color
+  }
   return (
     <article>
-      
+      <header className='account-card-header' style={headerColorStyle}>
+        <h3>Account #{id}</h3>
+      </header>
+      <p><span className="account-info-label">Account Title:</span> {title}</p>
+      <p><span className="account-info-label">Type:</span> {type}</p>
     </article>
   )
 }
