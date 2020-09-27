eb5c9853f674d3ecb17c9093aa1ae64c29be9706
Antonio Fry <antoniofry2000@gmail.com>
2020-09-27 13:04:20 -0600

    Render CategoryCards

--- src/Components/CategoryCard/CategoryCard.js
+++ src/Components/CategoryCard/CategoryCard.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import './CategoryCard.css';
 
-const CategoryCard = () => {
+const CategoryCard = ({ id, type, color }) => {
+  const backgroundColor = {
+    'backgroundColor': color
+  }
   return (
-    <article className="category-card">
+    <article className="category-card" style={backgroundColor}>
       <header>
-        <h3>Category #{}</h3>
+        <h3 className='category-title'>Category: {type}</h3>
       </header>
+      <div className='category-card-btn-container'>
+        <button className='category-card-btn'>Edit</button>
+        <button className='category-card-btn'>Remove</button>
+      </div>
     </article>
   )
 }
