284a25ebdc1e5a1a1e4fdaaf159840606b237358
Antonio Fry <antoniofry2000@gmail.com>
2020-09-27 12:40:41 -0600

    Build basic layout for CategoryCard

---
+++ src/Components/CategoryCard/CategoryCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const CategoryCard = () => {
+  return (
+    <article className="category-card">
+      <header>
+        <h3>Category #{}</h3>
+      </header>
+    </article>
+  )
+}
+
+export default CategoryCard;
