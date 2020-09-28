774ad6c72241091f666a5aa1be80571fa235ca73
Antonio Fry <antoniofry2000@gmail.com>
2020-09-27 18:09:11 -0600

    put content on landing page

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import './LandingPage.css';
+import { landingPageData } from './landingPageData';
+var demoGif = require('./expense-tracker-demo.gif')
 
 const LandingPage = () => {
+  const mappedLinks = landingPageData.socialLinks.map(obj => {
+    return (
+      <a className='social-link' href={obj.link}><img className="social-image" src={obj.src} alt={obj.alt}/></a>
+    )
+  })
   return (
     <section className="landing-page-container">
       <article className="project-info-container">
-
+        <div className="text-container">
+          <h2>Introduction</h2>
+          <p className="intro-text">{landingPageData.introduction}</p>
+        </div>
+        <div className="gif-container">
+          <img src={demoGif} alt="demo of site" />
+        </div>
       </article>
       <footer className="external-links-container">
-
+        {mappedLinks}
       </footer>
     </section>
   )
 }
 
-export default LandingPage
+export default LandingPage;
---
+++ src/Components/LandingPage/landingPageData.js
@@ -0,0 +1,18 @@
+var githubIcon = require('./github.svg');
+var linkedinIcon = require('./linkedin-icon.svg')
+
+export const landingPageData = {
+  introduction: "Expense Tracker is a light built web application that allows users to add categorical, account, and expense data to a set of corresponding dashboards. To get started click on one of the navigation tabs at the top to see, edit and add on to pre-existing data",
+  socialLinks: [
+    { 
+      alt: 'Github icon',
+      src: githubIcon,
+      link: 'https://github.com/AntonioFry',
+    },
+    {
+      alt: 'LinkedIn icon',
+      src:  linkedinIcon,
+      link: 'https://www.linkedin.com/in/antonio-fry/'
+    }
+  ] 
+}
